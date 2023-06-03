6756ee8b9de3aba0ba5cd81980fbd62c672b8eb4
root <root@ip-172-31-6-157.ap-south-1.compute.internal>
2023-06-03 11:37:36 +0000

    instance with public_key

--- main.tf
+++ main.tf
@@ -1,18 +1,15 @@
-resource "aws_instance" "res1" {
-  ami           = "ami-078efad6f7ec18b8a"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "Node1"
-  }
+resource "aws_key_pair" "generated_key" {
+  key_name   = var.key_pair_name
+  public_key = var.public_key
 }
 
-resource "aws_instance" "res2" {
+resource "aws_instance" "res1"{
   ami           = "ami-078efad6f7ec18b8a"
   instance_type = "t2.micro"
+  key_name      = aws_key_pair.generated_key.key_name
   tags = {
-    Name = "Node2"
+    Name = "Node_key5"
   }
 }
-
 
 
---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "public_key" {
+  type    = string
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEo8nT6K1uSapgjeYWshn1PREwMRj0IQ3Cw5QyGXnJfnTVygIsik12EaapgciKk0fnVt+iR5z3mMzim2wwml8PGJ89Fwpvyy5t1AcNsSP/msoSKRS55Js0U9Y25WWAosnv8Y4AO6BX0NonhZzU3PjP5kw863PXZlXRDsgKmPt5nQ1xGaPdfoZjJh1KjjFS2dpnauwFfvE5oZpMARqBhIWx5LJdMCrmm9KIf87EQiQh6a1+DYRKGcOtZ6ZAOETo2ZsnrtPJ1hAfoWQ0OwXDvr8u+f1QCGlE5pJRi59Ffczuygc4/nzg8fOx0CRbitGhjxwFjRrlcqskFqPm81cObBHt DevOpsOperation1"
+}
+
+variable "key_pair_name" {
+  type    = string
+  default = "demokeypair"
+}
